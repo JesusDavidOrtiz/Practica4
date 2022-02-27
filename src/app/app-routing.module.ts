99603b69a9416553e7aee91138e96553f9379f87
Jesus David Ortiz Arevalo <davinchi89@hotmail.com>
2022-02-27 12:54:08 -0500

    Desarrollo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'header', 
+    loadChildren: () =>import('./header/header.module').then(m=>m.HeaderModule)
+  },
+  {
+     path: 'productos', 
+    loadChildren: () =>import('./productos/productos.module').then(m=>m.ProductosModule)
+  },
+  {
+     path: 'usuarios', 
+    loadChildren: () =>import('./usuarios/usuarios.module').then(m=>m.UsuariosModule)
+  },
+  {
+     path: 'ventas', 
+    loadChildren: () =>import('./ventas/ventas.module').then(m=>m.VentasModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { CategoriasComponent } from './categorias/categorias.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { HeaderComponent } from './header/header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriasComponent,
+    ClienteComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
